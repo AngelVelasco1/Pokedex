e2df61176c6a851b5ef04fc5afb23548cde46065
Angel David Velasco <82744167+AngelVelasco1@users.noreply.github.com>
2023-04-21 09:03:59 -0500

    feat: :zap: Improve performance of pokemon info lateral card

--- components/data-API.js
+++ components/data-API.js
@@ -1,58 +1,52 @@
 let pokemons = [' '];
 
-window.addEventListener("DOMContentLoaded",  async () => {
-        let urlAPI = "https://pokeapi.co/api/v2/pokemon/?limit=1200";
-        let response = await fetch(urlAPI);
-        let responseAsJson = await response.json();
-    
-        for (let i = 0; i < responseAsJson.results.length; i++) {
-            pokemons.push({
-                id: i + 1,
-                name: responseAsJson.results[i].name,
-                types: []
-            });
-        };
-        getAllTypes();  
+window.addEventListener("DOMContentLoaded", async () => {
+    let urlAPI = "https://pokeapi.co/api/v2/pokemon/?limit=1200";
+    let response = await fetch(urlAPI);
+    let responseAsJson = await response.json();
+
+    for (let i = 0; i < responseAsJson.results.length; i++) {
+        pokemons.push({
+            id: i + 1,
+            name: responseAsJson.results[i].name,
+            types: []
+        });
+    };
+    getAllTypes();
 })
 
 async function getAllTypes() {
-  const requests = [];
+    const requests = [];
 
-  for (let i = 1; i <= 18; i++) {
-    const urlAPI = `https://pokeapi.co/api/v2/type/${i}`;
-    requests.push(fetch(urlAPI));
-  }
+    for (let i = 1; i <= 18; i++) {
+        const urlAPI = `https://pokeapi.co/api/v2/type/${i}`;
+        requests.push(fetch(urlAPI));
+    }
 
-  const responses = await Promise.all(requests);
+    const responses = await Promise.all(requests);
 
-  const responsesAsJson = await Promise.all(responses.map((response) => response.json()));
+    const responsesAsJson = await Promise.all(responses.map((response) => response.json()));
 
-  responsesAsJson.forEach((responseAsJson) => {
-    const pokemonInType = responseAsJson.pokemon;
+    responsesAsJson.forEach((responseAsJson) => {
+        const pokemonInType = responseAsJson.pokemon;
 
-    pokemonInType.forEach((pokemon) => {
-      const pokemonId = pokemon.pokemon.url.replace('https://pokeapi.co/api/v2/pokemon/', '').replace('/', '');
+        pokemonInType.forEach((pokemon) => {
+            const pokemonId = pokemon.pokemon.url.replace('https://pokeapi.co/api/v2/pokemon/', '').replace('/', '');
 
-      if (pokemonId <= pokemons.length && pokemons[pokemonId]) {
-        pokemons[pokemonId].types.push(responseAsJson.name);
-      }
+            if (pokemonId <= pokemons.length && pokemons[pokemonId]) {
+                pokemons[pokemonId].types.push(responseAsJson.name);
+            }
+        });
     });
-  });
 
-  loadingCompletion();
+    loadingCompletion();
 }
-
 
 function loadingCompletion() {
     let loadingDiv = document.getElementById('loading-div');
     loadingDiv.classList.add('hideLoading');
-
-    setTimeout(function() {
-        loadingDiv.classList.replace('hideLoading', 'hide');
-        document.body.style.overflow = 'unset';
-    }, 500);
-
-    pokemons.splice(0, 1);
-    currentList = pokemons;
+    /* Eliminar objeto vacio */
+    pokemons.shift();
+    currentList = pokemons.slice();
     updatePokemonList();
 };
