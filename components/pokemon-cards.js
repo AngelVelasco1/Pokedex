ae3e8e29bed11b9395367d049a98b703b24625a4
Angel Velasco <82744167+AngelVelasco1@users.noreply.github.com>
2023-04-25 11:02:05 -0500

    Update pokemon-cards.js

--- components/pokemon-cards.js
+++ components/pokemon-cards.js
@@ -1,7 +1,6 @@
-
 export default {
     showCards() {
-        let wk = new Worker("./storage/wkPokemon-cards.js");
+        let wk = new Worker("/storage/wkPokemon-cards.js");
 
         wk.addEventListener("message", (e) => {
         let pokemons = [' '];
